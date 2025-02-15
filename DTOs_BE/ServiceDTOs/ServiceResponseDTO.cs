69a56c514be732cf438c57c36e946f4b93fa0793
Hidden1201 <behien121@gmail.com>
2025-02-15 21:16:13 +0700

    ServiceList and config JWT

---
+++ DTOs_BE/ServiceDTOs/ServiceResponseDTO.cs
@@ -0,0 +1,22 @@
+﻿using DataObjects_BE.Entities;
+using DTOs_BE.ServiceOrderDTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTOs_BE.ServiceDTOs
+{
+    public class ServiceResponseDTO
+    {
+        public int ServiceId { get; set; }
+        public string ServiceName { get; set; }
+        public float ServicePrice { get; set; }
+        public string ServiceDescription { get; set; }
+        public float ServiceDuration { get; set; }
+        public DateTime ServiceCreateDate { get; set; }
+        public bool IsActive { get; set; }
+        public virtual ICollection<ServiceOrderResponseDTO> ServiceOrders { get; set; }
+    }
+}
---
+++ Services_BE/Services/ServiceService.cs
@@ -0,0 +1,37 @@
+﻿using AutoMapper;
+using DataObjects_BE.Entities;
+using DTOs_BE.ServiceDTOs;
+using DTOs_BE.ServiceOrderDTOs;
+using Repositories_BE.Interfaces;
+using Services_BE.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services_BE.Services
+{
+    public class ServiceService: IServiceService
+    {
+        private readonly IServiceRepositoy _serviceRepositoy;
+        private readonly IMapper _mapper;
+        public ServiceService(IServiceRepositoy serviceRepositoy, IMapper mapper) 
+        {
+            _serviceRepositoy = serviceRepositoy;
+            _mapper = mapper;
+        }
+
+        public async Task<List<ServiceResponseDTO>> ListService()
+        {
+            try
+            {
+                var list =  _serviceRepositoy.Get(includeProperties: "ServiceOrders").ToList();
+                if (list == null) { throw new Exception("List is empty"); }
+                var result = _mapper.Map<List<ServiceResponseDTO>>(list);
+                return result;
+            }catch (Exception ex) { throw ex; }
+        }
+
+    }
+}
