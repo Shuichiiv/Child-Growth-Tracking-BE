c37045d588c633ba368add384599a4cb72d2a700
Hidden1201 <behien121@gmail.com>
2025-02-16 02:33:07 +0700

    Set up JWT and Middle Ware

---
+++ WebAPI_BE/Controllers/ServiceController.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Services_BE.Interfaces;
+
+namespace WebAPI_BE.Controllers
+{
+    [Route("api/service")]
+    [ApiController]
+    public class ServiceController : ControllerBase
+    {
+        private readonly IServiceService _service;
+        private readonly IMapper _mapper;
+        public ServiceController(IServiceService service, IMapper mapper)
+        {
+            _service = service;
+            _mapper = mapper;
+        }
+        [HttpGet("list-service")]
+        public async Task<IActionResult> GetListServices()
+        {
+            var response = await _service.ListService();
+            if (response == null )
+            {
+                return NotFound();
+            }
+            return Ok(response);
+        }
+    }
+}
