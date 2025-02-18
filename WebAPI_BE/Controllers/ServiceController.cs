26b67ca6c199f53cbb709f357dcb518868513c14
Hidden1201 <behien121@gmail.com>
2025-02-18 23:53:51 +0700

    .

--- WebAPI_BE/Controllers/ServiceController.cs
+++ WebAPI_BE/Controllers/ServiceController.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using DTOs_BE.ServiceDTOs;
 using Microsoft.AspNetCore.Mvc;
 using Services_BE.Interfaces;
 
@@ -24,6 +25,45 @@ namespace WebAPI_BE.Controllers
                 return NotFound();
             }
             return Ok(response);
+        }
+        [HttpGet("get-service-by-id/{id}")]
+        public async Task<IActionResult> GetServiceById(int id)
+        {
+            var response = await _service.GetServiceById(id);
+            return Ok(response);
+        }
+        [HttpPost("create-service")]
+        public async Task<IActionResult> CreateService([FromBody] CreateServiceModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            try
+            {
+                var serviceResponse = await _service.CreateService(model);
+                return Ok(serviceResponse);
+            }catch (Exception ex)
+            {
+                return StatusCode(500, $"Internal server error: {ex.Message}");
+            }
+        }
+        [HttpPut("update-service/{id}")]
+        public async Task<IActionResult> UpdateService([FromBody] UpdateServiceModel model, int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            try
+            {
+                var serviceResponse = await _service.UpdateService(model, id);
+                return Ok(serviceResponse);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Internal server error: {ex.Message}");
+            }
         }
     }
 }
