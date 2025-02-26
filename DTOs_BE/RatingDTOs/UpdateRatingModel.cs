4609879ac23e67ad90ecf1f71c21ec46e4ad0a07
Hidden1201 <behien121@gmail.com>
2025-02-26 22:58:38 +0700

    CRUD Rating

---
+++ DTOs_BE/RatingDTOs/UpdateRatingModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTOs_BE.RatingDTOs
+{
+    public class UpdateRatingModel
+    {
+        public double RatingValue { get; set; }
+    }
+}
